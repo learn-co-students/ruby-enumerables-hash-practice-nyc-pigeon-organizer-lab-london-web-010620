fd5dfcef9860982fab2fd36bceb66f833a7cdd3c
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-29 10:28:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |info, value|
+    value.each do |stats, names|
+      names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][info] == nil
+          pigeon_list[name][info] = []
+        end
+      pigeon_list[name][info].push(stats.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
